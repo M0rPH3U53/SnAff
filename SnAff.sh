c3ffb276fe178abf593dd9c9491b128d8cec1745
M0rPH3U53 <144029241+M0rPH3U53@users.noreply.github.com>
2026-03-02 11:02:40 +0100

    Update SnAff.sh

--- SnAff.sh
+++ SnAff.sh
@@ -1,13 +1,48 @@
 #!/bin/bash
 
-# Cherche & filtre les service Bonjour/ZeroCOnf
-sniff=$(avahi-browse -a -r -t | grep -E "(hostname|address|port|txt)" | awk '{print $3}' | paste - - - -)
+# Banniere
+cat <<"EOF"
 
-# Verifie si la variable est vide, affiche & enregistre les resultats
+MP""""""`MM          MMP"""""""MM .8888b .8888b 
+M  mmmmm..M          M' .mmmm  MM 88   " 88   " 
+M.      `YM 88d888b. M         `M 88aaa  88aaa  
+MMMMMMM.  M 88'  `88 M  MMMMM  MM 88     88     
+M. .MMM'  M 88    88 M  MMMMM  MM 88     88     
+Mb.     .dM dP    dP M  MMMMM  MM dP     dP     
+MMMMMMMMMMM          MMMMMMMMMMMM   
+
+by M0rPH3U53
+      
+EOF
+
+# Couleur ASCII
+BLEU='\033[34m'
+ROUGE='\033[0;31m'
+VERT='\033[0;32m'
+GRIS='\033[0;90m'
+RESET='\033[0m'
+BLANC='\033[1;37m'
+JAUNE='\033[0;33m'
+
+# Cherche & filtre les services
+echo -ne "👻 ${BLANC}Scan${RESET}..."
+sniff=$(avahi-browse -a -r -t | grep -E "(hostname|address|port|txt)" | awk '{print $3}' | paste - - - -)
+echo -e "${JAUNE}100%${RESET}"
+
+# Chemin du fichier sauvegarder
+dir=$(pwd)
+
+echo -e " " 
+
+# Verifie si la varibale est vide & affiche les services
 if [[ -z ${sniff} ]]; then
    echo "❌ Aucun services trouvé"
    exit 1
 else
-   echo "👻 ${sniff}"
+   echo -e "${VERT}[+]${RESET} ${BLANC}Services${RESET}"
+   echo " "
+   echo "${sniff}"
    echo "${sniff}" > SnAff.txt
+   echo " "
+   echo -e "${VERT}[+]${RESET} Sauvegardé --> "${dir}"/SnAff.txt"
 fi
